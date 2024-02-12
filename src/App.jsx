033c9de59b4b4d1f739afe0e78ac769ebf668fee
Salman-sajib <sfsajib@gmail.com>
2024-02-12 13:29:49 +0600

    created the AddTodo component and finish wiriting code in it

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className='text-5xl'>Learn Readux Toolkit</h1>
+      <h1 className='text-5xl text-red-700'>Learn Readux Toolkit</h1>
     </div>
   );
 }

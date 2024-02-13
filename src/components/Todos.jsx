97a80f5674a6b16b230a862b44a06fc5f5f0dd41
Salman-sajib <sfsajib@gmail.com>
2024-02-13 10:41:26 +0600

    added the functionality to delete an item from the list

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { removeTodo } from '../features/todo/todoSlice';
 
 function Todos() {
   const todos = useSelector((state) => state.todos);
-  console.log(todos);
+  const dispatch = useDispatch();
 
   return (
     <div>
       <h1>Todos</h1>
       {todos.map((todo) => (
-        <div key={todo.id}>{todo.text}</div>
+        <li key={todo.id}>
+          {todo.text}
+          <button
+            className='ml-3'
+            onClick={() => dispatch(removeTodo(todo.id))}
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </div>
   );
